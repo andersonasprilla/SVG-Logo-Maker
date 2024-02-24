204df970fad3a1d1f5443a9fe57936f5b4662408
Anderson Asprilla <anderson.asprilla@gmail.com>
2024-02-23 22:58:03 -0500

    generate Svg

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
+const generateSvg = require('./lib/generateSvg')
 
 const questions = [
     {
+        type: 'input',
+        message: 'Enter up to 3 characters',
+        name: 'chars'
+    },
+    {
+        type: 'input',
+        message: 'Type the text color',
+        name: 'textColor'
+    },
+    {
         type: 'list',
         message: 'what shape ?',
-        name: 'shapes',
+        name: 'shape',
         choices: ['square', 'rectangle', 'circle', 'triangle']
-    }
+    },
+    {
+        type: 'input',
+        message: 'what shapes color ?',
+        name: 'shapeColor',
+    },
 ]
 
 const writeToFile = (fileName, data) => {
@@ -23,7 +39,8 @@ const init = () => {
     inquirer
     .prompt(questions)
     .then((answers) => {
-        writeToFile('./examples/logo.svg', answers.shapes)       
+        const renderShape = generateSvg(answers)
+        writeToFile('./examples/logo.svg', renderShape)       
     })
 }
 
